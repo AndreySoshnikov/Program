2f1b8882276042b42a31194fe183c3ed31c8fb91
Andrey Soshnikov <Solllnikov@mail.ru>
2023-01-23 23:00:09 +0300

    Добавил решение 15 задачи второго семинара

---
+++ Seminar2Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день 
+//недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+System.Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 0 && day < 6)
+{
+    System.Console.WriteLine("Нет, этот день не является выходным");
+}
+else if (day == 6 || day == 7)
+{
+    System.Console.WriteLine("Да, этот день выходной");
+}
+else
+{
+    System.Console.WriteLine("Такого дня не существует");
+}
